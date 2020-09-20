8f917683dacbb9d00117b7c9f30626c9729a5991
templatewebsite <71581352+templatewebsite@users.noreply.github.com>
2020-09-20 11:34:50 +0530

    Add files via upload

---
+++ validation/Validation.js
@@ -0,0 +1,30 @@
+﻿function Required(ctrlID, ctrlName) {
+    var txtControl = document.getElementById(ctrlID);
+    var string = document.getElementById(ctrlID).value;
+    var spaceCount;
+
+    if (txtControl.value == '') {
+        alert('Please ' + ctrlName + '.');
+        txtControl.focus();
+
+        return false;
+    }
+    else {
+        spaceCount = 0;
+        for (var count = 0; count < string.length; count++) {
+            var ch = string.substring(count, count + 1);
+
+            if (ch == ' ') {
+                spaceCount++;
+            }
+        }
+        if (spaceCount == string.length) {
+            alert('Please Enter ' + ctrlName + '.');
+            txtControl.value = "";
+            txtControl.focus();
+            return false;
+        }
+    }
+
+    return true;
+}
